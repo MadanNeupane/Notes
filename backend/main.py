a151a57f100654883890968ea1e5f8929c3051a4
Madan Neupane <madanneupane09@gmail.com>
2024-09-19 18:15:27 +0545

    feat: test home route

---
+++ backend/main.py
@@ -0,0 +1,12 @@
+from config import app, db
+
+@app.route("/", methods=["GET"])
+def index():
+    return "Start of the Levo assignment"
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all() # Create database when app starts
+
+    app.run(debug=True)

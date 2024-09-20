d827bcbf4c2d14351ad516ee28305ff8ff1e596b
Madan Neupane <madanneupane09@gmail.com>
2024-09-20 12:53:52 +0545

    feat: add note model

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,9 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime, timezone
 from config import db
 
+def lazy_utc_now():
+    return datetime.now(tz=timezone.utc)
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -21,4 +24,26 @@ class User(db.Model):
             'id': self.id,
             'username': self.username,
             'email': self.email
+        }
+
+
+class Note(db.Model):
+    __tablename__ = 'notes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=lazy_utc_now)
+    updated_at = db.Column(db.DateTime, default=lazy_utc_now, onupdate=lazy_utc_now)
+    reminder_time = db.Column(db.DateTime, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship('User', backref='notes')
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'content': self.content,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at,
+            'reminder_time': self.reminder_time,
+            'user_id': self.user_id
         }

8996717e883130d0ccf5c983c59f911969605d97
Madan Neupane <madanneupane09@gmail.com>
2024-09-21 06:05:57 +0545

    init: frontend starter files with react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <h1>Start of the Levo assignment - Frontend</h1>
+      </div>
+    </>
+  )
+}
+
+export default App

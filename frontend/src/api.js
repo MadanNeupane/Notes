a93da3b75dad0ccaf24a4b3d2bf7eaeb3a504907
Madan Neupane <madanneupane09@gmail.com>
2024-09-22 01:11:00 +0545

    temp: baseurl test for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
 //   baseURL: "http://localhost:5000",
-  baseURL: import.meta.env.VITE_API_URL,
+  // baseURL: import.meta.env.VITE_API_URL,
+  baseURL: '/', // for production test
 });
 
 api.interceptors.request.use((config) => {

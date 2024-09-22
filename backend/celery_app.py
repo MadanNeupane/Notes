3a72b2b444d95cec06aebcc6802aa1968efbcf44
Madan Neupane <madanneupane09@gmail.com>
2024-09-22 18:16:57 +0545

    fix: utc timezone on reminder time

--- backend/celery_app.py
+++ backend/celery_app.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_mail import Mail, Message
 from config import db, CELERY_BROKER_URL, CELERY_RESULT_BACKEND, MAIL_USERNAME
 from models import Note, User, Reminder
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 app = Flask(__name__)
 
@@ -17,7 +17,7 @@ mail = Mail(app)
 # Define the periodic task
 @celery.task
 def send_reminder_email_task():
-    current_time = datetime.utcnow()
+    current_time = datetime.now(timezone.utc)
     upcoming_minute = current_time + timedelta(minutes=1)
 
     # Query notes with reminder_time within the next minute
@@ -31,7 +31,7 @@ def send_reminder_email_task():
         send_reminder_email(user.email, note.content, note.id)
 
 def send_reminder_email(email, content, note_id):
-    note_url = f"{os.getenv('FRONTEND_DOMAIN')}/notes/{note_id}"
+    note_url = f"{os.getenv('FRONTEND_DOMAIN')}/notes"
     msg = Message('Reminder: Your Note', sender=MAIL_USERNAME, recipients=[email])
     msg.body = f"Here is your reminder:\n\n{content}\n\nView your note here: {note_url}"
 
@@ -42,7 +42,7 @@ def send_reminder_email(email, content, note_id):
         reminder = Reminder.query.filter_by(note_id=note_id).first()
         if reminder:
             reminder.is_sent = True
-            reminder.last_delivered_at = datetime.utcnow()
+            reminder.last_delivered_at = datetime.now(timezone.utc)
             db.session.commit()
 
         print(f"Reminder email sent to {email}")
